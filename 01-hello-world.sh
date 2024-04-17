952843e909dd47b4362906f86b56a42d46232efa
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-17 20:25:19 +0530

    01-hello-world.sh

---
+++ 01-hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh::Hello suresh,How are you?"
+echo "suresh::Hi Ramesh,Iam fine How are you?"
+echo "Ramesh :Iam fine too,How is your work?"
+echo "suresh"::not bad Iam thinking to upgrade to Devops"
