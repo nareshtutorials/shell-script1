bd1d7ab517b58e97f06c616e4eb8276d5f51d9e7
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-18 20:06:42 +0530

    script is addes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,4 +2,4 @@
 echo "Ramesh::Hello suresh,How are you?"
 echo "suresh::Hi Ramesh,Iam fine How are you?"
 echo "Ramesh :Iam fine too,How is your work?"
-echo "suresh"::not bad Iam thinking to upgrade to Devops"
+echo "suresh::not bad Iam thinking to upgrade to Devops"
