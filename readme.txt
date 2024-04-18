b1b6525249831a147e95482ef6528307cdb6fe0a
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-18 22:20:18 +0530

    modified file

--- readme.txt
+++
@@ -1,7 +0,0 @@
-**steps:**
-*create repo in Github
-*clone repo in local laptop
-*start developing
-*Add the files in temporary area
-*commit changes
-*push the changes to Github/central repos
---
+++ variables.sh
@@ -0,0 +1,11 @@
+#Declaring variable
+
+person1=$naresh
+petrson2=$suresh
+
+#Referring variable
+
+echo "$person1::Hii $person2 how are you?"
+echo "$person2::Hii $person1 iam fine, How are you?"
+echo "$person1::Iam fine too,how is your work"
+echo "$person2::Not bad.Iam thinking to upgrade devops"
