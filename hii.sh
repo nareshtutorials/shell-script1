e7bf514756fa6d7e64ffcefdfbb584828120bbf2
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-20 07:53:28 +0530

    file.sh added

---
+++ hii.sh
@@ -0,0 +1,14 @@
+* shell scripts
+**steps:**
+*create a repo in Github
+*clone repo in local laptop
+*start developing
+*Add files to temporary area
+....
+git add <file-name>
+......
+
+*commit the changes
+
+*
+*push the changes to Github/central repo
