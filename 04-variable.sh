78e6b9efdab95fb225f2a6cad169177fe65dce8a
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-21 00:20:31 +0530

    files are added

--- 04-variable.sh
+++ 04-variable.sh
@@ -5,7 +5,7 @@ person2=$2
 
 #Reffering Variable
 
-echo "person1::Hello person2,How are you?"
-echo "person2::Hii person1,Iam fine.How are you?"
-echo "person1::Iam fine too.How is your Work?"
-echo "person2::not bad.Iam thinking to upgrade Devops."
+echo "$person1::Hello $person2,How are you?"
+echo "$person2::Hii $person1,Iam fine.How are you?"
+echo "$person1::Iam fine too.How is your Work?"
+echo "$person2::not bad.Iam thinking to upgrade Devops."
