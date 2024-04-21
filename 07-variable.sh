cd9bc892d6dedd0958e6ebc17c42a48117a6b7bc
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-21 11:35:52 +0530

    file is added

--- 07-variable.sh
+++ 07-variable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Movies =("Dj Tillu","Dj","Dubai seenu")
+Movies =("DjTillu","Dj","Dubaiseenu")
 #size of the above array is 3
 #Indexing starts with 0,1,2
 echo "First movie is :${Movies[0]}"
